44026b61b6f4d0fd0dd74ca06f7ba2b0c891064a
Mohamed Arfath <mohamedarfath62@gmail.com>
2026-08-07 23:05:41 +0530

    Change Meta API version to v20.0 to fix delivery failure

--- routers/whatsapp.py
+++ routers/whatsapp.py
@@ -101,7 +101,7 @@ def send_meta_reply(to_phone: str, reply_text: str):
     phone_id = os.getenv("PHONE_NUMBER_ID", PHONE_NUMBER_ID)
     token = os.getenv("WHATSAPP_TOKEN", WHATSAPP_TOKEN)
     
-    url = f"https://graph.facebook.com/v25.0/{phone_id}/messages"
+    url = f"https://graph.facebook.com/v20.0/{phone_id}/messages"
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json"
